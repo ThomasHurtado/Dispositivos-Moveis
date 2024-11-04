bd0747afb8cfbbd3cdea7255f37815af7ea325ce
ThomasHurtado <thomashurtado@outlook.com.br>
2024-11-04 14:01:17 -0300

    Inicialização

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: '@react-native',
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'semi': 'off', // Desativa a verificação de ponto e vírgula
+  },
+};
